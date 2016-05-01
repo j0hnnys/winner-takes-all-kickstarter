4cd811c2e194ffc7f5a540164c7a906c1e7e9f0e
brandonzhen <bzijiez@gmail.com>
2016-05-01 11:07:48 -0700

    Added incomplete template

--- server/main.js
+++ server/main.js
@@ -1,7 +1,18 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { Template } from 'meteor/templating';
 
 export const Tasks = new Mongo.Collection('tasks');
+
+Template.body.helpers({
+  projects: [
+    { projectName: 'First Project' },
+    { projectName: 'Second Project' },
+    { projectName: 'Third Project' },
+    { projectName: 'Fourth Project' },
+    { projectName: 'Fifth Project' },
+  ],
+});
 
 Meteor.startup(() => {
   // code to run on server at startup
