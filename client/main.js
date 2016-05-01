3c7e69f28ef1c4619c751f0cda023cf2a94cb1d3
brandonzhen <bzijiez@gmail.com>
2016-05-01 11:54:28 -0700

    Added template

--- client/main.js
+++ client/main.js
@@ -2,10 +2,43 @@ FlowRouter.route('/', {
   action: function (params) {
     BlazeLayout.render("main", {
       area: "main"
-      });
+    });
   }
 });
 
 if (Meteor.isClient) {
-
+  Template.main.helpers({
+    projects: [
+      {
+        projectName: 'Oculus Rift',
+        imageName: '"vr_oculus.png"',
+        priceTag: '$0.99',
+        numBackers: '15',
+      },
+      {
+        projectName: 'FOVE',
+        imageName: '"vr_fove.jpg"',
+        priceTag: '$1.00',
+        numBackers: '12',
+      },
+      {
+        projectName: 'Wearality',
+        imageName: '"vr_wearality.jpg"',
+        priceTag: '$69.69',
+        numBackers: '31',
+      },
+      {
+        projectName: 'HTC Vive',
+        imageName: '"vr_htc.jpg"',
+        priceTag: '$420.00',
+        numBackers: '6',
+      },
+      {
+        projectName: 'HoloLense',
+        imageName: '"vr_microsoft_hololense.jpg"',
+        priceTag: '$9001.69',
+        numBackers: '18',
+      },
+    ],
+  });
 }
--- server/main.js
+++ server/main.js
@@ -4,16 +4,6 @@ import { Template } from 'meteor/templating';
 
 export const Tasks = new Mongo.Collection('tasks');
 
-// Template.body.helpers({
-//   projects: [
-//     { projectName: 'First Project' },
-//     { projectName: 'Second Project' },
-//     { projectName: 'Third Project' },
-//     { projectName: 'Fourth Project' },
-//     { projectName: 'Fifth Project' },
-//   ],
-// });
-
 Meteor.startup(() => {
   // code to run on server at startup
 });
