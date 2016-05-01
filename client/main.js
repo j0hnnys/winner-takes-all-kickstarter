10c2b8b1ec0b8083e5d9e9ec70a56f363d4fdfe7
brandonzhen <bzijiez@gmail.com>
2016-05-01 12:02:34 -0700

    Template img src fix

--- client/main.js
+++ client/main.js
@@ -11,31 +11,31 @@ if (Meteor.isClient) {
     projects: [
       {
         projectName: 'Oculus Rift',
-        imageName: '"vr_oculus.png"',
+        imageName: 'vr_oculus.png',
         priceTag: '$0.99',
         numBackers: '15',
       },
       {
         projectName: 'FOVE',
-        imageName: '"vr_fove.jpg"',
+        imageName: 'vr_fove.jpg',
         priceTag: '$1.00',
         numBackers: '12',
       },
       {
         projectName: 'Wearality',
-        imageName: '"vr_wearality.jpg"',
+        imageName: 'vr_wearality.jpg',
         priceTag: '$69.69',
         numBackers: '31',
       },
       {
         projectName: 'HTC Vive',
-        imageName: '"vr_htc.jpg"',
+        imageName: 'vr_htc.jpg',
         priceTag: '$420.00',
         numBackers: '6',
       },
       {
         projectName: 'HoloLense',
-        imageName: '"vr_microsoft_hololense.jpg"',
+        imageName: 'vr_microsoft_hololense.jpg',
         priceTag: '$9001.69',
         numBackers: '18',
       },
