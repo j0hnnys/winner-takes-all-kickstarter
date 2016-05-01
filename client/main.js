d33db3a983d202018823d8796961f499ef65b19f
brandonzhen <bzijiez@gmail.com>
2016-05-01 13:53:12 -0700

    Show most backers first*

--- client/main.js
+++ client/main.js
@@ -36,7 +36,7 @@ if (Meteor.isClient) {
 
   Template.main.helpers({
     projects() {
-      return ProjectDB.find({}, { sort: { createdAt: -1 } });	// reverse search
+      return ProjectDB.find({}, { sort: { numBackers: -1 } });	// most backers first
     },
   });
 }
@@ -49,34 +49,34 @@ if (Meteor.isClient) {
 //   projectName: 'Oculus Rift',
 //   imageName: 'vr_oculus.png',
 //   priceTag: '$0.99',
-//   numBackers: '15',
+//   numBackers: 15,
 //   description: 'This is a short description.',
 // });
 // db.ProjectDB.insert({
 //   projectName: 'FOVE',
 //   imageName: 'vr_fove.jpg',
 //   priceTag: '$1.00',
-//   numBackers: '12',
+//   numBackers: 12,
 //   description: 'This is a short description.',
 // });
 // db.ProjectDB.insert({
 //   projectName: 'Wearality',
 //   imageName: 'vr_wearality.jpg',
 //   priceTag: '$69.69',
-//   numBackers: '31',
+//   numBackers: 31,
 //   description: 'This is a short description.',
 // });
 // db.ProjectDB.insert({
 //   projectName: 'HTC Vive',
 //   imageName: 'vr_htc.jpg',
 //   priceTag: '$420.00',
-//   numBackers: '6',
+//   numBackers: 6,
 //   description: 'This is a short description.',
 // });
 // db.ProjectDB.insert({
 //   projectName: 'HoloLense',
 //   imageName: 'vr_microsoft_hololense.jpg',
 //   priceTag: '$9001.69',
-//   numBackers: '18',
+//   numBackers: 18,
 //   description: 'This is a short description.',
 // });
